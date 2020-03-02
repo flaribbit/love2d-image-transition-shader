f92c2a4796e93bce9893757dccb19dcaa2866497
flaribbit <1149761294@qq.com>
2020-03-02 17:50:19 +0800

    init

---
+++ main.lua
@@ -0,0 +1,43 @@
+local newImage=love.graphics.newImage
+local setShader=love.graphics.setShader
+local rect=love.graphics.rectangle
+local draw=love.graphics.draw
+
+local imgTransition=newImage("transition.png")
+local imgList={
+    newImage("1.jpg"),
+    newImage("2.jpg"),
+}
+
+local shader=love.graphics.newShader[[
+extern Image imgTransition;
+extern number time;
+vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords)
+{
+    vec4 c=Texel(texture, texture_coords);
+    if(time<Texel(imgTransition, texture_coords).r){
+        color.a=0.;
+    }
+    return c;
+}
+]]
+
+local Frames=0
+
+function love.load()
+    -- shader:send("imgTransition",imgTransition)
+    print(shader)
+end
+
+function love.update()
+    Frames=Frames+1
+end
+
+function love.draw()
+    -- love.graphics.setColor(1,1,1)
+    draw(imgList[1],0,0,0,720/1080)
+    setShader(shader);
+    shader:send("time",Frames%60/60)
+    draw(imgList[2],0,0,0,720/1080)
+    setShader()
+end
